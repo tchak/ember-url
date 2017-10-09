9009598d3225c1e411dc631d9f63163a3a2ac4e8
Paul Chavard <git@paul.chavard.net>
2017-10-09 10:39:33 +0300

    update ember-cli

--- testem.js
+++ testem.js
@@ -3,12 +3,25 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'PhantomJS'
+    'PhantomJS',
+    'Chrome',
+    'Firefox'
   ],
   launch_in_dev: [
     'PhantomJS',
     'Chrome',
     'Safari',
     'Firefox'
-  ]
+  ],
+  browser_args: {
+    Firefox: [
+      '-headless'
+    ],
+    Chrome: [
+      '--disable-gpu',
+      '--headless',
+      '--remote-debugging-port=9222',
+      '--window-size=1440,900'
+    ]
+  }
 };
