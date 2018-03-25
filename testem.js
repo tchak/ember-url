ba278c5c68c78fbf5eb249d8da9c2181ebc35e35
Paul Chavard <git@paul.chavard.net>
2018-03-25 20:20:40 +0200

    Remove PhantomJS tests

--- testem.js
+++ testem.js
@@ -1,8 +1,8 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['PhantomJS', 'Chrome', 'Firefox'],
-  launch_in_dev: ['PhantomJS', 'Chrome', 'Safari', 'Firefox'],
+  launch_in_ci: ['Chrome', 'Firefox'],
+  launch_in_dev: ['Chrome', 'Safari', 'Firefox'],
   browser_args: {
     Firefox: {
       mode: 'ci',
